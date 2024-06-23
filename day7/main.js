c61f59b2fdeb1dd38242a9b86cc8daeb19c0b689
virtualabishek <717abishekneupane@gmail.com>
2024-06-23 02:52:38 +0000

    Day8

---
+++ day7/main.js
@@ -0,0 +1,44 @@
+function innerbu() {
+document.getElementById("inner").innerHTML = "You clicked this." 
+}
+function writebu() {
+    document.write(5+6)
+}
+function winbu() {
+    window.alert("You are at danger.")
+}
+
+//Console
+console.log("Full Stack Devloper");
+//Full Console
+var i = 10;
+console.log(i)
+var i = i+ i;
+console.log(i);
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+console.log(cars[0])
+
+var x = 16;
+var f = typeof("Hello World")
+console.assert.log(f)
+
