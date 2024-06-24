aa1bf11b3e750aee5da044b85059bbf96f6257ed
virtualabishek <717abishekneupane@gmail.com>
2024-06-24 09:35:18 +0000

    Day4

---
+++ day9/main.js
@@ -0,0 +1,14 @@
+function add() {
+    document.getElementById("main").innerHTML=(3+4)
+}
+function addi() {
+    alert(4+6)
+}
+
+const car = {type: "honda", year: "1990", method: "XR" }
+car.new = 2029;
+
+const person = new Object();
+person.name = "Abishek";
+person.address = "Gorkha"
+person.age = 21;
