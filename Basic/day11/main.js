7330a671d63bb4f8d05e6a27c6fc85d39343f72b
virtualabishek <717abishekneupane@gmail.com>
2024-06-26 08:21:14 +0000

    Change

---
+++ Basic/day11/main.js
@@ -0,0 +1,16 @@
+// This is  inline comment
+/* 
+We all will be bro*/
+var a = 10; //Inline comment
+
+var b = null;
+let d = "Abishek";
+const e = "Nepal";
+
+c = 10 + 10;
+
+g = 10.23;
+
+var firstName = "Abishek";
+var lastName = "Neupane";
+console.log(firstName)
--- day11/main.js
+++
@@ -1,4 +0,0 @@
-const first = ["Nepal", "Canada", "UK", "USA", "Japan", "Malasiya" ];
-let fi = first[3];
-leng = first.at[2]
-console.log(leng);
