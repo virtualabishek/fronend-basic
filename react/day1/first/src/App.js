e3c72191082ef693bef972ba002d1f8f8801455d
virtualabishek <717abishekneupane@gmail.com>
2024-06-27 00:58:41 +0000

    ch

--- react/day1/first/src/App.js
+++ react/day1/first/src/App.js
@@ -5,8 +5,15 @@ function Header() {
   return <h1>This is heading.</h1>;
 }
 
-function App() {
-  return <Header /> 
-}
+function App() { 
+  return ( 
+    <div className="App"> 
+      This is the starting code for "Your first component" ungraded lab 
+      <Header/> 
+    </div> 
+  ); 
+} 
+ 
+
 
 export default App;
