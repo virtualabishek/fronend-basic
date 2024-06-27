7a4ab687c779e9dc8ebf363b54d199ddf192ab5a
virtualabishek <717abishekneupane@gmail.com>
2024-06-27 00:31:52 +0000

    Change

---
+++ react/day1/first/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return <h1>This is heading.</h1>;
+}
+
+function App() {
+  return <Header /> 
+}
+
+export default App;
