bac1c6eb198f37028cf8abe035bc6ab125391048
virtualabishek <717abishekneupane@gmail.com>
2024-07-09 08:42:45 +0545

    Changing All

---
+++ Basic/day12/main.js
@@ -0,0 +1,15 @@
+const myNumber = 10;
+
+
+const myFlo = 39.32;
+console.log(myNumber)
+
+console.log(myFlo)
+
+console.log( myNumber === 10)
+
+const myString = "5221"
+console.log(Boolean(false))
+
+console.log("Hello WOrld!")
+
