71b48bf8de4471fcdd4a5263bb889b8fbdf6987e
virtualabishek <717abishekneupane@gmail.com>
2024-07-10 16:00:45 +0545

    Done

---
+++ Basic/day14/main.js
@@ -0,0 +1,13 @@
+const band = {
+    vocals: "Robert Plant", 
+    guitar: "Jimmy Page",
+    bass: "John Paul Jones",
+    drums: "John Bonham"
+};
+
+// Destructure Objects
+
+const {vocals, guitar, bass, drumns} = band;
+
+console.log(vocals)
+console.log(guitar)
