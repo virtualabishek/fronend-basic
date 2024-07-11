7fc5eef28e14103631cdfefa8638a436750df87d
virtualabishek <717abishekneupane@gmail.com>
2024-07-11 21:10:48 +0545

    DOM

---
+++ Basic/dom/main.js
@@ -0,0 +1,12 @@
+const view1 = document.getElementById("view1");
+console.log(view1);
+
+const view2 = document.querySelector("#view2");
+console.log("view2")
+
+view1.style.display = "felx"
+view2.style.display = "none"
+
+
+const views = document.getElementsByClassName("view")
+console.log(views)
