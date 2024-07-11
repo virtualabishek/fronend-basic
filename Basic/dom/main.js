fb7fdc08ebfcfad0c027a6814690daa66675e76d
virtualabishek <717abishekneupane@gmail.com>
2024-07-11 21:30:21 +0545

    JS is fun

--- Basic/dom/main.js
+++ Basic/dom/main.js
@@ -10,3 +10,42 @@ view2.style.display = "none"
 
 const views = document.getElementsByClassName("view")
 console.log(views)
+
+const sameViews = document.querySelectorAll('.views');
+console.log(sameViews)
+
+const divs = view1.querySelectorAll("div");
+
+console.log(divs)
+
+const sameDivs = view1.getElementsByTagName("div")
+console.log(sameDivs)
+
+const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)");
+console.log(evenDivs)
+for (let i = 0; i<evenDivs.length; i++) {
+    evenDivs[i].style.backgroundColor = "darkblue";
+    /* evenDivs[i].style.width="200px";
+    evenDivs[i].style.height="200px"; */
+}
+
+const myText = document.querySelector("nav h1")
+console.log(myText)
+myText.textContent = "Hello World!"
+const navbar = document.querySelector("nav")
+navbar.innerHTML = `<h1>Hello</h1> <p>This should align write.`
+console.log(navbar)
+navbar.style.justifyContent = "space-between";
+
+console.log(evenDivs[0]);
+console.log(evenDivs[0].parentElement);
+console.log(evenDivs[0].parentElement.children);
+console.log(evenDivs[0].parentElement.childNodes);
+console.log(evenDivs[0].parentElement.hasChildNodes);
+console.log(evenDivs[0].parentElement.lastChild);
+console.log(evenDivs[0].parentElement.lastElementChild);
+console.log(evenDivs[0].parentElement.firstChild);
+console.log(evenDivs[0].parentElement.firstElementChild);
+console.log(evenDivs[0].nextSibling);
+console.log(evenDivs[0].nextElementSibling);
+
