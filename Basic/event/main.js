61f299e1034a0e730b0c4051876e38a23c8a0f6a
virtualabishek <717abishekneupane@gmail.com>
2024-07-13 12:26:15 +0545

    day

--- Basic/event/main.js
+++ Basic/event/main.js
@@ -1,4 +1,13 @@
 // Event Listener
 
-const view = document.querySelector("view2");
-console.log(view)
+const view = document.querySelector("#view2");
+const div = view.querySelector("div");
+const h2 = div.querySelector("h2");
+
+// call event
+
+const doSomethings = () => {
+    alert("Doing Something");
+}
+
+h2.addEventListener("click", doSomethings, false)
