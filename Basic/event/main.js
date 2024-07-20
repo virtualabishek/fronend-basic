7246894e1d196c2031c4d8356165dcabc5e91694
virtualabishek <717abishekneupane@gmail.com>
2024-07-20 05:51:29 +0545

    Done

--- Basic/event/main.js
+++ Basic/event/main.js
@@ -1,16 +1,2 @@
-// Event Listener
-
-const view = document.querySelector("#view2");
-const div = view.querySelector("div");
-const h2 = div.querySelector("h2");
-
-// call event
-
-const doSomethings = () => {
-    alert("Doing Something");
-}
-
-h2.addEventListener("click", doSomethings, false)
-
-
-//This is all about Elite
+const view = document.querySelector("view1");
+console.log(view)
