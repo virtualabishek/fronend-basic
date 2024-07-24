7469708013182751101c35b986a2e4eef7228ec8
virtualabishek <717abishekneupane@gmail.com>
2024-07-24 14:13:48 +0545

    Day 16

---
+++ next_js/my-app/src/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Home() {
+  const data = [
+    {
+      name : "1"
+    },
+    {
+      name: "2"
+    }
+  ]
+  return (
+   <div>
+    {
+// data.map 
+// data.filter 
+
+    }
+   </div>
+  );
+}
