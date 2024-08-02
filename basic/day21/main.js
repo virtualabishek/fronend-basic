b054a93ae37490791bf753fd6f03cb917db68fa2
virtualabishek <717abishekneupane@gmail.com>
2024-08-02 06:57:12 +0545

    day20

---
+++ basic/day21/main.js
@@ -0,0 +1,35 @@
+// function pow(x, n) {
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+
+//   return result;
+// }
+
+// Object the baisc
+
+
+// let user = {
+//   name: "Abishek",
+//   roll: 1,
+//   sem: "3rd",
+//   age: 21,
+//   "likes birds": true
+// };
+
+// // Object { key: value, key: "value"}
+
+// console.log(user.name);
+// console.log(user.age )
+
+// user.isAdmin = true;
+
+// console.log(user.isAdmin);
+
+// delete user.sem;
+
+// console.log(user["like birds"]);
+
+// asking input from user and making a 
