61bd50180b76db09c3e3b0c298c171b2d974d34a
virtualabishek <717abishekneupane@gmail.com>
2024-08-06 13:20:27 +0545

    started react

---
+++ basic/day24/main.js
@@ -0,0 +1,17 @@
+console.log("okey, finish all part - 1")
+
+// prototype & inheritence
+
+function sayHi() {
+    console.log("Hi.")
+}
+
+window.sayHi();
+
+document.body.style.background = "red";
+setTimeout(() => document.body.style.background = "", 1000);
+
+// alert(location.href);
+// if(confirm("Go to wikipedia?")){
+//     location.href = "https://wilipedia.org"
+// }
--- react/day1/first/src/App.js
+++ react/day1/first/src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header() {
-  return <h1>This is heading.</h1>;
+function Mybutton(){
+  return (
+    <button>This is a button</button>
+  );
 }
-
 function App() { 
-  return ( 
-    <div className="App"> 
-      This is the starting code for "Your first component" ungraded lab 
-      <Header/> 
-    </div> 
-  ); 
+  return (
+    <>
+    <h1>Hello World!</h1>
+    <p>You are learning, React? right</p>
+    <Mybutton />
+    </>
+  );
 } 
  
 
