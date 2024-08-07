133d2c08dc36d6c4f219fba21fe879c3bcc8b52d
virtualabishek <717abishekneupane@gmail.com>
2024-08-07 13:00:36 +0545

    going

--- react/day3/src/App.js
+++ react/day3/src/App.js
@@ -1,19 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  const namehandle = () => {
-    const name = ["Abishek", "Abinash", "Bhagawati"];
-    const newa = Math.floor(Math.random() * name.length);
-    return name[newa];
-  };
+//Checking Onclic ...
 
+function MyButton() {
+  const [count, setCount] = useState(0);
+  function check() {
+    setCount(count+1)
+  }
   return (
-    <div className="appMain">
-      <p>Hello World!</p>
-      <p>Hello {namehandle()}</p>
+    <>
+    <button onClick={check}>You clicked me: {count} times.</button> 
+    </>
+  );
+}
+//Fixing on from react.dev. :)))
+
+export function Bio() {
+  const name = "Abishek Neupane";
+  return(
+    <>
+    <div className="intro">
+      <h1>Welcome to my website.</h1>
     </div>
+    <p className="summary">
+      You can find my thoughts here. -by {name} <br /><br />
+      <b>And<i>pictures</i></b> of scientists!
+    </p>
+    </>
   );
 }
 
-export default App;
+// displaying time
+
+export default function App() {
+  return (
+    <>
+    <MyButton/>
+    <Bio />
+    </>
+  );
+}
