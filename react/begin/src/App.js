68da15fa1f4bab876a62d3ee574d93c07d566ab3
virtualabishek <717abishekneupane@gmail.com>
2024-08-08 09:48:29 +0545

    started

--- react/begin/src/App.js
+++ react/begin/src/App.js
@@ -1,28 +1,7 @@
-
-import './App.css';
-import { useReducer } from 'react';
-import profile from './profile.jpg';
-import Header from './component/Header';
-
-const user = {
-  name: "Abishek Devloper",
-  img: "profile.jpg"
-
-}
-function MyButton() {
+export default function App() {
   return (
-  <button className='button1'>My button</button>
-  );
-}
-
-
-
-function App() {
-  return (
-    <>    
-    <Header />
+    <>
+    
     </>
   );
 }
-
-export default App;
