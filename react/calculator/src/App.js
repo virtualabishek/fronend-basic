f6d9835400086a970b3a8f581087f531f89829bc
virtualabishek <717abishekneupane@gmail.com>
2024-08-11 19:06:08 +0545

    done

--- react/calculator/src/App.js
+++ react/calculator/src/App.js
@@ -1,42 +1,43 @@
-function Item({ name, isPacked }) {
-  return (
-    <li>
-      {isPacked ? (
-        <del>{name + "/"}</del>
-      ) : (
-        name
-      )}
-    </li>
-  );
-}
+// function Item({ name, isPacked }) {
+//   return (
+//     <li>
+//       {isPacked ? (
+//         <del>{name + "/"}</del>
+//       ) : (
+//         name
+//       )}
+//     </li>
+//   );
+// }
 
-export function PackingList() {
-  return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item 
-          isPacked={true} 
-          name="Space suit" 
-        />
-        <Item 
-          isPacked={true} 
-          name="Helmet with a golden leaf" 
-        />
-        <Item 
-          isPacked={false} 
-          name="Photo of Tam" 
-        />
-      </ul>
-    </section>
-  );
-}
+// export function PackingList() {
+//   return (
+//     <section>
+//       <h1>Sally Ride's Packing List</h1>
+//       <ul>
+//         <Item 
+//           isPacked={true} 
+//           name="Space suit" 
+//         />
+//         <Item 
+//           isPacked={true} 
+//           name="Helmet with a golden leaf" 
+//         />
+//         <Item 
+//           isPacked={false} 
+//           name="Photo of Tam" 
+//         />
+//       </ul>
+//     </section>
+//   );
+// }
 
-export default function App() {
-  return (
-    <>
-      <h1>Day 4 - Going</h1>
-      <PackingList />
-    </>
-  );
-}
+// export default function App() {
+//   return (
+//     <>
+//       <h1>Day 4 - Going</h1>
+//       <PackingList />
+//     </>
+//   );
+// }
+
