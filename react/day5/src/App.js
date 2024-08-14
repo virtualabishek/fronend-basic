c7230f4aa26f3edfce3f47d00c7c1e1e8aa504b8
virtualabishek <717abishekneupane@gmail.com>
2024-08-14 20:58:52 +0545

    going

--- react/day5/src/App.js
+++ react/day5/src/App.js
@@ -2,17 +2,26 @@ import { useState } from "react";
 import {sculptureList} from './data';
 export function Practice1() {
   const [index, setIndex] = useState(0);
+  const [showMore, SetShowMore] = useState(false)
   function handleClick() {
     setIndex(index+1);
+  }
+  function handleShowMoreClick(){
+    SetShowMore(!showMore);
   }
   let sculpture = sculptureList[index];
   return(
     <>
     <button onClick={handleClick}>Next</button>
     <h2><i>{sculpture.name}</i> by {sculpture.artist}</h2>
-    <h3>({index + 1} of {sculptureList.length})</h3>
-    <img src={sculpture.url} alt={sculpture.alt} />
-    <p>{sculpture.description}</p>
+    <h3>{index + 1} of {sculptureList.length}</h3>
+    <button onClick={handleShowMoreClick}>{showMore ? 'Hide' : 'Show'} details</button>
+    {showMore && <p>{sculpture.description}</p>}
+    <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+
     </>
   );
 }
@@ -20,6 +29,7 @@ export default function App(){
   return(
     <>
     <h1>Checking...</h1>
+    <Practice1/>
     <Practice1/>
     </>
   );
