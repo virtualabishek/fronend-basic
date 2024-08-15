2c505dc0f87f528f2161203d9afc9c8bb9795080
virtualabishek <717abishekneupane@gmail.com>
2024-08-15 13:42:05 +0545

    going

--- react/day6/src/App.js
+++ react/day6/src/App.js
@@ -1,39 +1,75 @@
 import { useState } from "react";
 // Render and commit the changes
 
-export function Clock({time}){
-  return (
-    <div>
-      <h1>Current Time: {time}</h1>
-    </div>
-  );
-}
-export function Form() {
-  const [isSent, setIsSent] = useState(false);
-  const [message, setMessage] = useState("Hi!");
-  if(isSent){
-    return <h1>Your message is on its way.</h1>
+// export function Clock({time}){
+//   return (
+//     <div>
+//       <h1>Current Time: {time}</h1>
+//     </div>
+//   );
+// }
+// export function Form() {
+//   const [isSent, setIsSent] = useState(false);
+//   const [message, setMessage] = useState("Hi!");
+//   if(isSent){
+//     return <h1>Your message is on its way.</h1>
+//   }
+//   return (
+//     <>
+//     <h4>Form</h4>
+//     <form onSubmit={(e) => {
+//       e.preventDefault();
+//       setIsSent(true);
+//       setMessage(message);
+//     }}>
+//       <textarea name="name" id="name"
+//       placeholder="Message"
+//       value={message}
+//       onChange={(e)=> setMessage(e.target.value)}>
+        
+//       </textarea><br></br>
+//       <button type="submit">Send</button>
+//     </form>
+//     </>
+//   );
+// }
+
+export function Form(){
+  const [toSent, setToSent] = useState('Ram');
+  // suru ma default type ko diyo. (Ram)
+  const [message, setMessage] = useState('Hello');
+  //default message ->Hello.
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTimeout(()=> {
+      alert(`You send: ${message} to ${toSent}`);
+    },5000)
   }
   return (
     <>
-    <h4>Form</h4>
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      setIsSent(true);
-      setMessage(message);
-    }}>
-      <textarea name="name" id="name"
-      placeholder="Message"
+    <form onSubmit={handleSubmit}>
+      <label>
+        To: {' '}
+        <select value={toSent}
+        onChange={e=>setToSent(e.target.value)}>
+          <option value="Ram">Ram</option>
+          <option value="Shyam">Shyam</option>
+          <option value="Others">Others</option>
+        </select>
+      </label>
+      <br/>
+      <br/>
+      <textarea 
+      placeholder="Message..."
       value={message}
-      onChange={(e)=> setMessage(e.target.value)}>
-        
-      </textarea><br></br>
+      onChange={e=> setMessage(e.target.value)}/>
+      <br/>
+      <br/>
       <button type="submit">Send</button>
     </form>
     </>
   );
 }
-
 export default function App(){
   return (
     <>
