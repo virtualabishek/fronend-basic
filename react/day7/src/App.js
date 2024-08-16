6ffe7dd0bf835ec9e30a4e980c62efae651a3fd9
virtualabishek <717abishekneupane@gmail.com>
2024-08-16 14:21:49 +0545

    going

--- react/day7/src/App.js
+++ react/day7/src/App.js
@@ -1,3 +1,4 @@
+import { type } from "@testing-library/user-event/dist/type";
 import { useState } from "react";
 let nextId = 0;
 export function List(){
@@ -52,12 +53,66 @@ export function List1(){
     </>);
 
 }
+//Transfering of an Array
+let initialShapes = [
+    { id: 0, type: 'circle', x: 50, y: 100 },
+    { id: 1, type: 'square', x: 150, y: 100 },
+    { id: 2, type: 'circle', x: 250, y: 100 },
+  ];
+  
+  export function ShapeEditor() {
+    const [shapes, setShapes] = useState(
+      initialShapes
+    );
+  
+    function handleClick() {
+      const nextShapes = shapes.map(shape => {
+        if (shape.type === 'square') {
+          // No change
+          return shape;
+        } else {
+          // Return a new circle 50px below
+          return {
+            ...shape,
+            y: shape.y + 50,
+          };
+        }
+      });
+      // Re-render with the new array
+      setShapes(nextShapes);
+    }
+  
+    return (
+      <>
+        <button onClick={handleClick}>
+          Move circles down!
+        </button>
+        {shapes.map(shape => (
+          <div
+            key={shape.id}
+            style={{
+            background: 'purple',
+            position: 'absolute',
+            left: shape.x,
+            top: shape.y,
+            borderRadius:
+              shape.type === 'circle'
+                ? '50%' : '',
+            width: 20,
+            height: 20,
+          }} />
+        ))}
+      </>
+    );
+  }
+  
 
 export default function App() {
     return(
         <>
         <List/>       
         <List1/>       
+        <ShapeEditor/>
         </>
     );
 }
