b00a99590271515b2addf83ca473269285fe0883
virtualabishek <717abishekneupane@gmail.com>
2024-08-17 10:55:43 +0545

    done for now

--- react/day8/src/App.js
+++ react/day8/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function Form() {
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('typing');
+
+  const handleTextAreaChange = (e) => {
+    setAnswer(e.target.value);
+  };
+
+  if (status === 'success') {
+    return <h1>Form Submitted.</h1>;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('submitting');
+    try {
+      await submitForm(answer);
+      setStatus('success');
+    } catch (err) {
+      setStatus('typing');
+      setError(err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Fill the form!</h2>
+      <p>Tell me about your birthplace?</p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextAreaChange}
+          disabled={status === 'submitting'}
+        /> <br />
+        <button disabled={answer.length === 0 || status === 'submitting'}>
+          Submit
+        </button>
+        {error && <p className="showError">{error.message}</p>}
+      </form>
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <Form />
+    </>
+  );
+}
+
+function submitForm(answer) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'gorkha';
+      if (shouldError) {
+        reject(new Error('Good guess, but wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
+}
