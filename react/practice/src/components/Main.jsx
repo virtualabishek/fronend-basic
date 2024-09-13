46b3b36b774655fd0be432129b28ecd8bfdd7dad
virtualabishek <717abishekneupane@gmail.com>
2024-09-13 14:26:34 +0545

    done.

---
+++ react/practice/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released is 2013</li>
+        <li>Was Originally created by Jordan Walkie</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook.</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Main;
