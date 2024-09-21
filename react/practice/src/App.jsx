d14716b70d4644795e6283517849d792beceea56
virtualabishek <717abishekneupane@gmail.com>
2024-09-21 12:54:08 +0545

    going

--- react/practice/src/App.jsx
+++ react/practice/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import Demo from "./components/Demo";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 export default function App() {
   return (
     <>
-      <Navbar />
-      <Main />
+      {/* <Navbar />
+      <Main /> */}
+      <Demo />
     </>
   );
 }
---
+++ react/practice/src/components/Demo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Demo = () => {
+  return (
+    <div>
+      <h1>Just For Practice.</h1>
+    </div>
+  );
+};
+
+export default Demo;
