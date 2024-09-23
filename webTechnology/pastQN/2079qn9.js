ed69c4852e54e72aed1b58944891174334d3bc1b
virtualabishek <717abishekneupane@gmail.com>
2024-09-23 13:52:25 +0545

    done

--- webTechnology/pastQN/2079qn9.js
+++ webTechnology/pastQN/2079qn9.js
@@ -3,5 +3,24 @@
 //  of checkbox of 3 hobbies having at least one of them
 // to be checked.
 
-const password = document.getElementById("password");
-const checkList = document.getElementById("select");
+function validateForm() {
+  password = document.getElementById("passWord").value;
+  email = document.getElementById("emailID").value;
+  if (!email.includes("@") || !email.includes(".")) {
+    alert("Enter a valid email.");
+    return false;
+  }
+  if (password.length < 6) {
+    alert("Password should be greater than 6 characters.");
+    return false;
+  }
+  if (
+    !document.getElementById("hobby1").checked &&
+    !document.getElementById("hobby2").checked &&
+    !document.getElementById("hobby3").checked
+  ) {
+    alert("Please select at least one hobby.");
+    return false;
+  }
+  return true;
+}
