1b7c9ea608560af08e94bf90e8152dd5feed18f7
virtualabishek <717abishekneupane@gmail.com>
2024-09-23 06:07:41 +0545

    completed morning session

---
+++ webTechnology/pastQN/2079qn8.js
@@ -0,0 +1,10 @@
+// Write a JavaScript function that will include the variable having values number 5 and character ‘5’ and print the variables.
+
+function callMe(integer, character) {
+  console.log(integer);
+  console.log(character);
+  //   console.log(typeof integer);
+  //   console.log
+  //   (typeof character);
+}
+callMe(5, "5");
---
+++ webTechnology/pastQN/2079qn9.js
@@ -0,0 +1,7 @@
+// Write a JavaScript function to validate an email field,
+// password having length at least 6 and a collection
+//  of checkbox of 3 hobbies having at least one of them
+// to be checked.
+
+const password = document.getElementById("password");
+const checkList = document.getElementById("select");
