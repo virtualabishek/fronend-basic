cf11c0733456fb20a04227b74b55ac258212e7c0
virtualabishek <717abishekneupane@gmail.com>
2024-11-25 06:44:56 +0545

    2079 qn practice

---
+++ webTechnology/pastQN/2079/temp2.js
@@ -0,0 +1,9 @@
+/* Write a JavaScript function that
+ will include the variable having values number 5 and character ‘5’ and print the variables. */
+function callMe() {
+	const num1 = 5;
+	const char1 = '5';
+	console.log(num1);
+	console.log(char1);
+}
+callMe();
