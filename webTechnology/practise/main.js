c7e674d775a6108704b93fbd2616f9604d20fda0
virtualabishek <717abishekneupane@gmail.com>
2024-11-27 12:57:41 +0545

    just 1 day

---
+++ webTechnology/practise/main.js
@@ -0,0 +1,10 @@
+function Student(Name, Age, DOB){
+	this.Name = Name;
+	this.Age=Age;
+	this.DOB=DOB;
+}
+const stu1 = new Student ("Ram", 20, "2003/02/03");
+console.log(stu1.Name);
+console.log(stu1.Age);
+console.log(stu1.DOB);
+
