879547c1a58ddfd548267754ad63ada8e2dd385b
virtualabishek <717abishekneupane@gmail.com>
2024-12-24 20:34:20 +0545

    Done

--- webTechnology/JS/main.js
+++
@@ -1,2 +0,0 @@
-console.log("Hi, from console.");
-console.log("I feel like i have readed all the JS before. hahahehehuhu. 😆😆");
--- webTechnology/pastQN/2079/temp.js
+++
@@ -1,9 +0,0 @@
-function callMe(int, char) {
-	console.log(int);
-	console.log(char);
-	console.log(typeof int);
-	console.log(typeof char);
-}
-
-callMe(5, "5");
-
