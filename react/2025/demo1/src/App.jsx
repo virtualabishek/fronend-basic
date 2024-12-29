d837834904f7b24c4a0f50209c888b3bf72c7004
virtualabishek <717abishekneupane@gmail.com>
2024-12-29 08:42:13 +0545

    Add Fourth component

--- react/2025/demo1/src/App.jsx
+++ react/2025/demo1/src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import First from "./components/First";
 import Second from "./components/Second";
 import Third from "./components/Third";
+import Fourth from "./components/Fourth";
 const App = () => {
   return (
     <div>
       {/* <First /> */}
       {/* <Second /> */}
-      <Third />
+      {/* <Third /> */}
     </div>
   );
 };
--- react/2025/demo1/src/components/Third.jsx
+++ react/2025/demo1/src/components/Third.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+// import imageLink from "../utils/util1";
+
+export function getUrl(person, size = "s") {
+  return "https://i.imgur.com/" + person.imageId + size + ".jpg";
+}
+
+function Avatar({ person, size }) {
+  return (
+    <img src={getUrl(person)} alt={person.name} width={size} height={size} />
+  );
+}
 
 const Third = () => {
   return (
-    <nav>
-      <div className="flex flex-wrap flex-row">
-        <div>Abishek Neupane</div>
-      </div>
-    </nav>
+    <div>
+      <Avatar
+        size={80}
+        person={{
+          name: "Aklilu Lemma",
+          imageId: "OKS67lh",
+        }}
+      />
+      <Avatar
+        size={50}
+        person={{
+          name: "Lin Lanying",
+          imageId: "1bX5QH6",
+        }}
+      />
+    </div>
   );
 };
 
