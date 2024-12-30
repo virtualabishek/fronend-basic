745c9208a7e5d84f0e730b9337166d022d1185b2
virtualabishek <717abishekneupane@gmail.com>
2024-12-30 18:47:18 +0545

    Not much focus

--- react/2025/demo1/src/App.jsx
+++ react/2025/demo1/src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import First from "./components/First";
 import Second from "./components/Second";
 import Third from "./components/Third";
-import Fourth from "./components/Fourth";
+// import Fourth from "./components/Fourth";
+import Fifth from "./components/Fifth";
 const App = () => {
   return (
     <div>
       {/* <First /> */}
       {/* <Second /> */}
       {/* <Third /> */}
-      <Fourth />
+      {/* <Fourth /> */}
+      <Fifth />
     </div>
   );
 };
---
+++ react/2025/demo1/src/components/Fifth.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Fifth = () => {
+  return (
+    <div>
+      <div>Life is hard</div>
+    </div>
+  );
+};
+
+export default Fifth;
